383fdd38a0779c8263437ed0c5c922f0f0032873
sazzad <sazzadador@gmail.com>
2025-03-18 20:12:24 +0600

    reverse 3 way string

--- reverseString.js
+++ reverseString.js
@@ -1,6 +1,18 @@
-const string = "hallow";
-let reverse = "";
-for (letter of string) {
-  reverse = letter + reverse;
-}
-console.log(reverse);
+// const string = "hallow";
+// let reverse = "";
+// for (letter of string) {
+//   reverse = letter + reverse;
+// }
+// console.log(reverse);
+
+// =======================
+
+const string2 = "sazzad";
+// let reverse ="";
+// for (let i=0 ; i < string2.length; i++ ){
+//     let letter = string2[i];
+//     reverse =  letter + reverse;
+//     console.log(string2[i])
+// }
+ const reverse = string2.split("").reverse().join("")
+console.log(reverse)
