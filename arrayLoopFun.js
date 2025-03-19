70f740fa1fee9a94206db19dbfc97cef1434fc8a
sazzad <sazzadador@gmail.com>
2025-03-20 02:36:32 +0600

    Array function traversing

---
+++ arrayLoopFun.js
@@ -0,0 +1,11 @@
+const sum = (a,b)=> a+b;
+const sub = (a,b)=> a-b;
+const mul = (a,b)=> a+b;
+const div = (a,b)=> a+b;
+const modulus = (a,b)=> a%b;
+const funcs = [sum,sub, mul,div,modulus]
+const a= 5 ;
+const b = 10;
+for (let i= 0; i< funcs.length;i++){
+console.log(funcs[i](a,b))
+}
