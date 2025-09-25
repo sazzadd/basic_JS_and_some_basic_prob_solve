695604b4c38a3bdb50c42a93cd2fcff93ab00b52
sazzad <sazzadador@gmail.com>
2025-09-25 20:39:01 +0600

    cheapestPhone

---
+++ convert_prob_js/chepestPhone.js
@@ -0,0 +1,32 @@
+const mobiles = [
+  { name: "Samsung", price: 20000, camera: "12mp", color: "black" },
+  { name: "xoami", price: 1800, camera: "12mp", color: "black" },
+  { name: "Oppo", price: 30000, camera: "12mp", color: "black" },
+  { name: "Iphone", price: 100000, camera: "12mp", color: "black" },
+  { name: "Walton", price: 31000, camera: "12mp", color: "black" },
+  { name: "HTC", price: 27000, camera: "12mp", color: "black" },
+];
+// function cheapestPhone(phones) {
+//    let lowest = phones[0];
+//   for (let phone in phones) {
+//     console.log(phone);
+//     if (phone.price < lowest.price) {
+//       lowest = phone;
+//     }
+//   }
+//   return lowest;
+// }
+// console.log(cheapestPhone(mobiles));
+function cheapestPhone(phones) {
+  let lowest = phones[0];
+  for (let phone of phones) {
+    if (lowest.price > phone.price) {
+      lowest = phone;
+    }
+  }
+
+  return lowest;
+}
+console.log(cheapestPhone(mobiles));
+
+// to
