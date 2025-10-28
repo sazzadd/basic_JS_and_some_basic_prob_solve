cf8e7dfb21c8b85910d9c41fb17e9dde176995b6
sazzad <sazzadador@gmail.com>
2025-10-28 07:45:16 +0600

    //get phone number

--- DOM-practice/bankApps/js/login.js
+++ DOM-practice/bankApps/js/login.js
@@ -2,6 +2,11 @@ console.log("hello");
 const loginBtn = document.getElementById("login-btn");
 console.log(loginBtn);
 loginBtn.addEventListener("click", function (event) {
+  // prevent reloading
   event.preventDefault();
   console.log("login btn click");
+
+  //get phone number
+  const phoneNumber = document.getElementById("Phone  number");
+  
 });
