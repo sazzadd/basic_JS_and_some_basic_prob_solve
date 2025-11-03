7ae1c5c3ffb0c5374aac967af9bfa40c6c761d74
sazzad <sazzadador@gmail.com>
2025-11-03 10:57:36 +0600

    login

--- DOM-practice/bankApps/js/login.js
+++ DOM-practice/bankApps/js/login.js
@@ -7,6 +7,20 @@ loginBtn.addEventListener("click", function (event) {
   console.log("login btn click");
 
   //get phone number
-  const phoneNumber = document.getElementById("Phone  number");
-  
+  const phoneNumber = document.getElementById("phone-number").value;
+  console.log(phoneNumber);
+  const pinNum = document.getElementById("pin-number").value;
+  const errorContainer = document.getElementById("error-conainer");
+  const errorMsg = document.createElement("p");
+  errorMsg.innerText = "phon or pin is not match";
+  errorContainer.appendChild(errorMsg);
+
+  console.log(phoneNumber);
+  console.log(pinNum);
+  if (phoneNumber === "01705" && pinNum === "1234") {
+    console.log("u are login ");
+    window.location.href = "home.html";
+  } else {
+    console.log("phon or pin is not match");
+  }
 });
