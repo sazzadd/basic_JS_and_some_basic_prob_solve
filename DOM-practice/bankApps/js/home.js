b603b3540a1882cd438a7c1223660561f6efe91c
sazzad <sazzadador@gmail.com>
2025-11-04 16:46:13 +0600

    added partially add money event handler

--- DOM-practice/bankApps/js/home.js
+++ DOM-practice/bankApps/js/home.js
@@ -4,4 +4,8 @@ document
   .addEventListener("click", function (event) {
     console.log("add money btn click");
     event.preventDefault();
+
+    const addMoneyInput = document.getElementById("add-money-input").value;
+    const addMoneyPin = document.getElementById("add-money-pin-number").value;
+    console.log(addMoneyInput);
   });
