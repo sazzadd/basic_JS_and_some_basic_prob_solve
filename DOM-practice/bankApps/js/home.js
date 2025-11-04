f021f9449b733ba89ece2708f45602478e3591e5
sazzad <sazzadador@gmail.com>
2025-11-04 16:36:26 +0600

    add money

---
+++ DOM-practice/bankApps/js/home.js
@@ -0,0 +1,7 @@
+console.log("home added");
+document
+  .getElementById("add-money")
+  .addEventListener("click", function (event) {
+    console.log("add money btn click");
+    event.preventDefault();
+  });
